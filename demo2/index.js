5ddbfbaecb1d036769994670e4e76d5d6be92a19
CoffeeWorm <878041280@qq.com>
2018-04-09 10:56:44 +0800

    initial commitcoment

---
+++ demo2/index.js
@@ -0,0 +1,8 @@
+var app =  new Vue({
+  el:'#app',
+  data: {
+    name:'pangwei',
+    sex: 'male',
+    age: 18
+  }
+});
---
+++ 入门第一课/js/index.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    name: 'abc',
+    sex: 'male',
+    age:18
+  }
+});
